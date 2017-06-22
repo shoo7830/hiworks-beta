2ff4f4e8eb923aa69811c58e55d07512dbdc2555
Aria Kim <shoo7830@gmail.com>
2017-06-22 13:37:26 +0900

    [dev] 복사본생성, gitignore편집, 좌메뉴 간소화

--- src/pages/write-approve.vue
+++ src/pages/write-approve.vue
@@ -3,16 +3,15 @@
 		<div >
 			<h2>결재 문서 작성</h2>
 			<img src="~assets/images/write.png" alt="">
-			<!-- <router-link to="/"><span class="prev"></span></router-link> -->
 			<router-link to="/list-approve"><span class="next"></span></router-link>
 		</div>
-		
 	</div>
 </template>
 <script>
 
 export default {
 	layout: 'hiworks',
+	
 	name: 'write-approve',
 	head: {
 		title: '하이웍스 - 결재 문서 작성'
@@ -20,3 +19,7 @@ export default {
 
 }
 </script>
+
+<style scoped>
+	
+</style>
